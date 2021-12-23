5a536e2494ed01f534f2a44e1192c1558e490c3c
evgeniy_ponomarev <jasonponomarev@gmail.com>
2021-12-23 15:34:02 +0300

    add statful service

---
+++ statefulservice/app.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://jason:12345678@db/microservices'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+data = {}
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    theme = db.Column(db.String(120), nullable=False)
+    genereted = db.Column(db.String(255), nullable=False)
+
+
+class GetTexts(Resource):
+    def get(self):
+        theme = request.args.get('theme')
+        text = Data.query.filter_by(theme=theme).first()
+        if text:
+            text = text.genereted
+            if theme in data.keys():
+                data[theme] += 1
+            else:
+                data[theme] = 1
+        
+            return text
+        else:
+            if theme in data.keys():
+                data[theme] += 1
+            else:
+                data[theme] = 1
+            return "this theme not used"
+
+
+class GetStats(Resource):
+    def get(self):
+        return data
+
+
+api.add_resource(GetTexts, "/gettexts/", "/gettexts/<string:theme>/")
+api.add_resource(GetStats, "/getstats/", "/getstats/")
+
+
+if __name__ == '__main__':
+    app.run()
